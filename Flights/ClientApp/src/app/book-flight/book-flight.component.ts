28f5d10279f569af25e1263cd2926dd714ec9ef8
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-27 11:53:29 +0530

    adding bookflight component and send flight Id parameter

---
+++ Flights/ClientApp/src/app/book-flight/book-flight.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-book-flight',
+  templateUrl: './book-flight.component.html',
+  styleUrls: ['./book-flight.component.css']
+})
+export class BookFlightComponent implements OnInit {
+  flightId: string = 'not loaded'
+  constructor(private route:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(p => this.flightId = p.get("flightId") ?? 'not passed')
+  }
+
+}
