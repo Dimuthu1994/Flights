f5ff6a426a1b5a6dd02d4749a2a58f2119509736
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-06-28 07:34:33 +0530

    create register-passenger component and Dto

---
+++ Flights/Controllers/PassengerController.cs
@@ -0,0 +1,26 @@
+﻿using Flights.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Flights.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class PassengerController : ControllerBase
+	{
+		static private IList<NewPassengerDto> Passengers = new List<NewPassengerDto>();
+
+		[HttpPost]
+		[ProducesResponseType(201)]
+		[ProducesResponseType(400)]
+		[ProducesResponseType(500)]
+		public IActionResult Register(NewPassengerDto dto)
+		{
+			Passengers.Add(dto);
+			System.Diagnostics.Debug.WriteLine(Passengers.Count);
+			return Ok();
+		}
+
+
+	}
+}
