64011f1e96ecffd9f3a78d2186bf6fda903f1c1e
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-07-04 17:10:41 +0530

    layout my bookings

---
+++ Flights/Controllers/BookingController.cs
@@ -0,0 +1,26 @@
+﻿using Flights.Data;
+using Flights.ReadModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flights.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class BookingController : Controller
+	{
+		private readonly Entities _entities;
+
+		public BookingController(Entities entities)
+		{
+			_entities = entities;
+		}
+
+		[HttpGet("{email}")]
+		[ProducesResponseType(500)]
+		[ProducesResponseType(400)]
+		[ProducesResponseType(typeof(IEnumerable<BookingRm>), 200)]
+		public ActionResult<IEnumerable<BookingRm>> List(string email)
+		{
+		}
+	}
+}
